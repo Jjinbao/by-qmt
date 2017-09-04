4a7d8fe08ddd41fdfa4f961aa17843c2c9ea24c0
王金宝 <wangjinbao@4zlink.com>
2017-09-04 18:03:52 +0800

    首页

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by dell on 2017-9-4.
+ */
+$(document).ready(function(){
+    $('#address').on('click',function(e){
+
+    })
+    $('#myCarousel').carousel({interval:5000});//每隔5秒自动轮播
+})
