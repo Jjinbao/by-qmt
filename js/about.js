2a1a189bc70a13c6cf9474cebb118214ab874404
jinbao <jinbaow@yeah.net>
2017-09-06 00:15:24 +0800

    积分商城页面

--- js/about.js
+++ js/about.js
@@ -1,48 +1,8 @@
-
-var initLeftMenu=function(){
-    $('#plat').attr('class','a-menu');
-    $('#ymy').attr('class','a-menu');
-    $('#question').attr('class','a-menu');
-    $('#study').attr('class','a-menu');
-
-    $('.platform').css('display','none');
-    $('.about-ymy').css('display','none');
-    $('.common-questions').css('display','none');
-    $('.appoint-study').css('display','none');
-}
-$('#plat').on('click',function(e){
-    if($('#plat').hasClass('a-menu-active')){
-        return;
-    }else{
-        initLeftMenu();
-        $('#plat').addClass('a-menu-active');
-        $('.platform').css('display','block');
+$('#Pagination').pagination({
+    pageCount: 8,
+    items_per_page:5,
+    jump: true,
+    callback:function(api){
+        console.log('1234')
     }
 });
-$('#ymy').on('click',function(e){
-    if($('#ymy').hasClass('a-menu-active')){
-        return;
-    }else{
-        initLeftMenu();
-        $('#ymy').addClass('a-menu-active');
-        $('.about-ymy').css('display','block');
-    }
-})
-$('#question').on('click',function(e){
-    if($('#question').hasClass('a-menu-active')){
-        return;
-    }else{
-        initLeftMenu();
-        $('#question').addClass('a-menu-active');
-        $('.common-questions').css('display','block');
-    }
-})
-$('#study').on('click',function(e){
-    if($('#study').hasClass('a-menu-active')){
-        return;
-    }else{
-        initLeftMenu();
-        $('#study').addClass('a-menu-active');
-        $('.appoint-study').css('display','block');
-    }
-})
--- js/marks.js
+++ js/marks.js
@@ -4,6 +4,22 @@ $(document).ready(function(){
 
     })
     $('#myCarousel').carousel({interval:5000});//每隔5秒自动轮播
+
+    $('.change').on('click',function(e){
+        window.location.href='./templates/marks/detail.html'
+    })
+
+    $('.minus').click(function(e){
+        if($('#buy_num').val()>0){
+            $('#buy_num').attr('value',parseInt($('#buy_num').val())-1);
+        }else{
+            return;
+        }
+    })
+
+    $('.plus').click(function(e){
+        $('#buy_num').attr('value',parseInt($('#buy_num').val())+1);
+    })
 })
 
 $('#Pagination').pagination({
