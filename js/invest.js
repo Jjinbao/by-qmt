1506267dba375bcc99f80c14ee9dbec4f499f4f5
王金宝 <wangjinbao@4zlink.com>
2017-09-06 21:22:59 +0800

    首页和详情

---
+++ js/invest.js
@@ -0,0 +1,12 @@
+try{
+    $('#Pagination').pagination({
+        pageCount: 8,
+        items_per_page:5,
+        jump: true,
+        callback:function(api){
+
+        }
+    });
+}catch (e){
+
+}
