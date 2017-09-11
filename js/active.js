a63e22684a3eb679244e7369208a95613ee634dd
王金宝 <wangjinbao@4zlink.com>
2017-09-11 16:12:20 +0800

    修改对照结果

--- js/active.js
+++ js/active.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
     $('#myCarousel').carousel({interval:5000});//每隔5秒自动轮播
+
+    //搜索事件
+    var clickEnterBtn=function(e){
+        if(!$('#search_name').val()){
+            return;
+        }
+        if(e.keyCode == '13'){
+            //$('#search_name').blur();
+            console.log('123');
+            window.location.href='#';
+        }
+    }
+
+    $('#search_name').focus(function(){
+        $(document).bind('keyup',clickEnterBtn);
+    });
+
+    $('#search_name').blur(function(){
+        $(document).unbind('keyup',clickEnterBtn);
+    });
 });
+
+
 try{
     $('#Pagination').pagination({
         pageCount: 8,
@@ -10,5 +32,7 @@ try{
             console.log('1234')
         }
     });
+} catch (e){
+    console.log('这里没有分页');
 }
 
