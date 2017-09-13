3beecbb9a3ef15670832a68ef07c57b952797ea3
王金宝 <wangjinbao@4zlink.com>
2017-09-13 15:51:07 +0800

    个人通知

---
+++ js/user-notice.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    try{
+        $('#Pagination').pagination({
+            pageCount: 4,
+            items_per_page:5,
+            jump: true,
+            callback:function(api){
+
+            }
+        });
+    }catch (e){
+
+    }
+})
