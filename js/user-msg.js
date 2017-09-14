93efedefb5e67a8fd6301e8216b0e62fb66756a0
王金宝 <wangjinbao@4zlink.com>
2017-09-14 18:36:57 +0800

    模态窗

--- js/user-msg.js
+++ js/user-msg.js
@@ -1,28 +1,32 @@
-$(document).on('opening', '.remodal', function () {
-    console.log('opening');
-});
-
-$(document).on('opened', '.remodal', function () {
-    console.log('opened');
-});
-
-$(document).on('closing', '.remodal', function (e) {
-    console.log('closing' + (e.reason ? ', reason: ' + e.reason : ''));
-});
-
-$(document).on('closed', '.remodal', function (e) {
-    console.log('closed' + (e.reason ? ', reason: ' + e.reason : ''));
-});
-
-$(document).on('confirmation', '.remodal', function () {
-    console.log('confirmation');
-});
-
-$(document).on('cancellation', '.remodal', function () {
-    console.log('cancellation');
-});
+// $(document).on('opening', '.remodal', function () {
+//     console.log('opening');
+// });
+//
+// $(document).on('opened', '.remodal', function () {
+//     console.log('opened');
+// });
+//
+// $(document).on('closing', '.remodal', function (e) {
+//     console.log('closing' + (e.reason ? ', reason: ' + e.reason : ''));
+// });
+//
+// $(document).on('closed', '.remodal', function (e) {
+//     console.log('closed' + (e.reason ? ', reason: ' + e.reason : ''));
+// });
+//
+// $(document).on('confirmation', '.remodal', function () {
+//     console.log('confirmation');
+// });
+//
+// $(document).on('cancellation', '.remodal', function () {
+//     console.log('cancellation');
+// });
 //模态框初始化
-var inst = $('[data-remodal-id=modal]').remodal();
+var inst = $('[data-remodal-id=modal1]').remodal();
+
+$('#loginWord').click(function(e){
+    inst.open();
+})
 
 $('.unbind').on('click',function(e){
     console.log(e.target.parentNode.parentNode);
